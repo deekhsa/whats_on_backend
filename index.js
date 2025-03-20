f17c09b1ccfed3f8947debc88371ff60cf81db27
deekhsa <deeksharv24@gmail.com>
2025-03-20 21:43:04 +0530

    Add dotenv support and update MongoDB connection

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-const DB_URI = "mongodb+srv://deeksha:1234@cluster0.juglg.mongodb.net/Personal?retryWrites=true&w=majority";
-
+const DB_URI = process.env.MONGO_URI;
 const app = express();
 
 mongoose.connect(DB_URI)
-
 const con = mongoose.connection;
 
-con.on('open', () => {
-    console.log("Connected");
-});
+con.on('open', () => console.log("Connected to MongoDB"));
 
-app.use(express.json())
+app.use(express.json());
 
-const restaurants = require('./routes/restaurant')
-app.use('/restaurants', restaurants)
+const restaurants = require('./routes/restaurant');
+app.use('/restaurants', restaurants);
 
-
-app.listen(9000, () =>{
-    console.log('server listening in port:9000')
-})
+app.listen(9000, () => console.log('Server listening on port 9000'));
